219309b1a3e0bd4030f937054347c55852aa6a0f
Daryl Teo <i.am@darylteo.com>
2012-09-27 23:52:43 +1000

    Initial commit

---
+++ src/DeploymentManager.java
@@ -0,0 +1,12 @@
+import org.vertx.java.deploy.Verticle;
+
+
+public class DeploymentManager extends Verticle {
+
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
